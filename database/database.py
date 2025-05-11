1f4a76848ee530b5df837b1e85d9e3167f1d1a33
Rafael <rafael.f.aria@hotmail.com>
2025-05-11 19:51:01 -0300

    atualizando

--- database/database.py
+++ database/database.py
@@ -2,6 +2,8 @@ from peewee import MySQLDatabase
 
 
 #db = MySQLDatabase('univesp_pi.db')
+#Banco de Dados Mysql online
+#mysql://root:GCruUsgoPnknKFPZLewtKKWftSVPNYtD@interchange.proxy.rlwy.net:45565/railway
 db = MySQLDatabase(
     'univesp_db',
     user='root',
@@ -9,3 +11,17 @@ db = MySQLDatabase(
     host='interchange.proxy.rlwy.net',
     port=45565 
 )
+"""CREATE TABLE itens_compra (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    compra_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    preco_unitario DECIMAL(10,2) NOT NULL,
+    qualidade_avaliada ENUM('Ruim', 'Regular', 'Bom', 'Ótimo'),
+    FOREIGN KEY (compra_id) REFERENCES compras(compra_id) ON DELETE CASCADE,
+    FOREIGN KEY (produto_id) REFERENCES produtos(produto_id) ON DELETE CASCADE
+);
+
+-- Atualizar a tabela fornecedores para armazenar a média de avaliações
+ALTER TABLE fornecedores ADD COLUMN avaliacao_media DECIMAL(3,2) DEFAULT 0;
+"""
