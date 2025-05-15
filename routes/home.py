25be41dac476a6e36d02f540a00d39ac2b38325f
Leticia Medeiros <leticiamedeirospereira@gmail.com>
2025-05-15 03:03:20 -0300

    testanto sqlconnector para a exibição das avaliações na página inicial

--- routes/home.py
+++ routes/home.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
-
+from database.sqlconnector import compras_positivas
 
 home_route = Blueprint('home', __name__)
 
 @home_route.route('/')
 def home():
     
-    return render_template('index.html')
+    return render_template('index.html', compras_positivas = compras_positivas)
