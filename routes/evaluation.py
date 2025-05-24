31e9470816daefefb741ca88fa394e0f715d3711
Rafael <rafael.f.aria@hotmail.com>
2025-05-24 10:39:39 -0300

    configurando avaliação

--- routes/evaluation.py
+++ routes/evaluation.py
@@ -1,6 +1,8 @@
 from flask import render_template, Blueprint, request
-
+from collections import defaultdict
 from database.models.evaluation import Evaluation
+from database.models.buy import Buy
+from database.models.product import Product
 
 evaluation_route = Blueprint('evaluation', __name__)
 
@@ -15,8 +17,9 @@ def insert_evaluation():
 
     new_evaluation = Evaluation.create(
         buy_id = data['buy_id'],
-        note=data['note'],
-        ranking = data['ranking']
+        product_id = data['product_id'],
+        quantidade_avaliada = data['quantidade_avaliada'],
+        qualidade_avaliada = data['qualidade_avaliada']
 
     )
 
@@ -24,13 +27,33 @@ def insert_evaluation():
 
 @evaluation_route.route('/new')
 def evaluation_form():
-    return render_template('evaluation/evaluation_form.html')
-
+    buys = Buy.select()
+    products_by_date = defaultdict(list)
+    # Agrupando os produtos por data de compra
+    # Para cada compra, adiciona o produto à lista correspondente à data
+    for buy in buys:
+        # Use str(buy.date) para garantir que a chave seja igual ao value do <option>
+        date_str = buy.date.strftime('%Y-%m-%d')
+        products_by_date[date_str].append({'id': buy.product.id, 'name': buy.product.name})
+    return render_template(
+        'evaluation/evaluation_form.html',
+        buys=buys,
+        products_by_date=products_by_date
+    )
 @evaluation_route.route('/<int:evaluation_id>/edit')
 def evaluation_edit_form(evaluation_id):
     evaluation = Evaluation.get_by_id(evaluation_id)
-
-    return render_template('evaluation/evaluation_form.html',evaluation=evaluation)
+    buys = Buy.select()
+    products_by_date = defaultdict(list)
+    for buy in buys:
+        date_str = buy.date.strftime('%Y-%m-%d')
+        products_by_date[date_str].append({'id': buy.product.id, 'name': buy.product.name})
+    return render_template(
+        'evaluation/evaluation_form.html',
+        evaluation=evaluation,
+        buys=buys,
+        products_by_date=products_by_date
+    )
 
 
 @evaluation_route.route('/<int:evaluation_id>/update', methods=['PUT'])
@@ -40,8 +63,9 @@ def update_evaluation(evaluation_id):
     evaluation_edited = Evaluation.get_by_id(evaluation_id)
 
     evaluation_edited.buy_id = data['buy_id']
-    evaluation_edited.note = data['note']
-    evaluation_edited.ranking = data['ranking']
+    evaluation_edited.product_id = data['product_id']
+    evaluation_edited.quantidade_avaliada = data['quantidade_avaliada']
+    evaluation_edited.qualidade_avaliada = data['qualidade_avaliada']
 
     evaluation_edited.save()
 
